9d160c12188b3d0967943331fab7ac5f6f47c319
Waliur Rahman <waliurrahman@Waliurs-MacBook-Air.local>
2025-10-19 18:17:24 +0600

    first commit

---
+++ testpython.py
@@ -0,0 +1,8 @@
+# Example 1: Loop through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Example 2: Loop using range()
+for i in range(5):
+    print("Number:", i)
